2868ccb2173f3441973655fedba209ae0ccbe4ad
Hugo Galán Cuevas <53835015+Hugogc@users.noreply.github.com>
2020-07-06 17:14:37 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  apiKey: '115480f20e96ed71f3249c137c7425cc',
+  apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   imgPath: 'https://image.tmdb.org/t/p'
 };
